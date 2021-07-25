d96df415a50a3d3ad3ba293527801706f5994b77
askschmidt <ask.schmidt@gmail.com>
2021-07-25 19:32:28 +0100

    Added relevant icon from API

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -28,12 +28,20 @@ function displayWeather(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute;
+  "alt", response.data.weather[0].descriptionn;
 }
 
 let apiKey = "f6e27edd666fe2993c6dd52e5b82732e";
