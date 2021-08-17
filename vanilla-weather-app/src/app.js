23dd95319f64a6d562fb50f653a84c89533a73da
askschmidt <ask.schmidt@gmail.com>
2021-08-17 20:42:47 +0100

    Added JS for looping through forecast

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -21,6 +21,36 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#weather-forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-2">
+  <div class="forecast-day">${day}</div>
+  <img
+  src="https://openweathermap.org/img/wn/01d@2x.png"
+  alt=""
+  width="50"
+  />
+  <div class="forecast-temperature">
+  <span class="forecast-temperature-max"> 18° </span>
+  <span class="forecast-temperature-min"> 12° </span>
+  </div>
+  </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -88,3 +118,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemp);
 
 search("Edinburgh");
+displayForecast();
