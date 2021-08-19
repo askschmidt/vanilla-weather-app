d9c6172790a380aa20d7cfebedd56b0a88e156ce
askschmidt <ask.schmidt@gmail.com>
2021-08-19 21:20:50 +0100

    updated wind speed unit

--- vanilla-weather-app/src/app.js
+++ vanilla-weather-app/src/app.js
@@ -87,8 +87,7 @@ function displayWeather(response) {
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML =
-    Math.round((response.data.wind.speed / 3.6) * 100) / 100;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
